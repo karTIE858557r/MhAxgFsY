bc692a9f869643b976facecb914961ce2ce39232
karTIE858557r <ratio.gathers_0c@icloud.com>
2025-03-27 19:23:25 +0000

    Add a user profile editing feature in the file

--- G8C/ETyT9/Msjf/2w0/academic-apps_Ip8mQq_1742511463.txt.py
+++ G8C/ETyT9/Msjf/2w0/academic-apps_Ip8mQq_1742511463.txt.py
@@ -11,5 +11,4 @@ def main():
     st.write('Welcome to our AI Research Summary Tool! Our app uses state-of-the-art AI technology to help healthcare professionals quickly and easily access and understand the latest research and findings related to healthcare.')
     st.write('Our tool is designed to convert research articles into a variety of formats that are easy to understand and consume. This tool will allow healthcare professionals to quickly and easily access and digest the latest research and findings, as well as create informative and engaging presentations for their colleagues and patients.')
     
-if __name__ == '__main__':
     main()
